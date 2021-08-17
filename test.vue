92ad2cf366a89c3dfde334449087c8e36afbf905
Naszrul Azreen Badarudin <nasxgeek@gmail.com>
2021-08-17 14:25:54 +0800

    Create test.vue

---
+++ test.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>Hello from Certainty</h1>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'ApplicationForm',
+    props: {
+        forUser: {
+            type: Boolean,
+        }
+    },
+    data(){
+        return{
+            isLoading: true
+        }
+    },
+    mounted() {
+    },
+    methods: {
+    }
+}
+</script>
